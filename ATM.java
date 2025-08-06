f2f919a36b3b74da7604834896715d2f63347538
Mohammad Kaif Ansari <kaifmumtajansari@gmail.com>
2025-08-06 08:05:25 +0530

    Update ATM.java

--- ATM.java
+++ ATM.java
@@ -1,9 +1,10 @@
 import java.io.IOException;
 public class ATM extends Optionmenu{
     public static void main(String[] args) throws IOException {
-        Optionmenu optionMenu = new Optionmenu(); 
+        Optionmenu optionMenu = new Optionmenu();  
 
         optionMenu.getLogin();
     }
 }
+
 
